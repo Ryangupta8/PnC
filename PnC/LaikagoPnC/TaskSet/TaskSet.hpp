b8111dde3fd5f3b4d4851b638c8c88f5bcaf3f71
Ryan Gupta <ryangupta8@gmail.com>
2020-07-17 10:46:43 -0500

    added a task set to LAikagoPnC

---
+++ PnC/LaikagoPnC/TaskSet/TaskSet.hpp
@@ -0,0 +1,7 @@
+#include <PnC/WBC/BasicTask.hpp>
+#include <PnC/LaikagoPnC/TaskSet/CoMxyzRxRyRzTask.hpp>
+#include <PnC/LaikagoPnC/TaskSet/FootRyRzTask.hpp>
+#include <PnC/LaikagoPnC/TaskSet/FootRzXYZTask.hpp>
+#include <PnC/LaikagoPnC/TaskSet/PointFootTask.hpp>
+#include <PnC/LaikagoPnC/TaskSet/SelectedJointTask.hpp>
+
